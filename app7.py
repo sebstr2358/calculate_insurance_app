d8bd942bd4add461245d386d727d059bcc42d9cc
sebstr2358 <sebix2358@onet.pl>
2025-05-16 17:29:14 +0200

    hide st.secrets

--- app7.py
+++ app7.py
@@ -22,26 +22,19 @@ CONVERT_TO_JSON_MODEL = "gpt-4o-mini"
 PREDICTION_CHARGE_MODEL = 'v4_insurance_charge_regression'
 CURRENCY = "USD"
 
-# Inicjalizacja stanu sesji
 if "openai_api_key" not in st.session_state:
     st.session_state["openai_api_key"] = ""
 
-if "input_given" not in st.session_state:
-    st.session_state["input_given"] = False
-
 # Prośba o podanie klucza API
 if st.session_state["openai_api_key"] == "":
-    if st.session_state["input_given"]:
-        st.success("Klucz API został pomyślnie zapisany.")
-    else:
-        st.warning("Dodaj swój klucz API OpenAI, aby móc korzystać z tej aplikacji:")
-        api_key_input = st.text_input("Klucz API", type="password")
+    st.warning("Dodaj swój klucz API OpenAI, aby móc korzystać z tej aplikacji:")
+    api_key_input = st.text_input("Klucz API", type="password")
 
-        # Jeśli klucz zostanie podany
-        if api_key_input:
-            st.session_state['openai_api_key'] = api_key_input  # Zapisanie klucza do sesji
-            st.session_state["input_given"] = True  # Ustawienie stanu
-            # Nie wywołuj st.experimental_rerun() tutaj
+    # Jeśli klucz zostanie podany
+    if api_key_input:
+        st.session_state['openai_api_key'] = api_key_input  # Zapisanie klucza do sesji
+        st.success("Klucz API został zapisany.")
+        st.experimental_rerun()  # Odśwież aplikację, aby zniknął input
 
 # Sprawdzenie dostępności klucza API
 if st.session_state["openai_api_key"] == "":
@@ -54,6 +47,7 @@ try:
 except Exception as e:
     st.error(f"Wystąpił błąd podczas inicjalizacji klienta OpenAI: {e}")
     st.stop()  # Zatrzymanie działania w przypadku błędu
+
 
 @st.cache_data
 def get_model():
